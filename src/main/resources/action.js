b822ebb130019a76e51a198764c37694127e20d1
Andres Marcelo <felipemarcelo156@gmail.com>
2020-09-01 19:32:51 -0500

    No se que estoy haiendo 2

--- src/main/resources/action.js
+++ src/main/resources/action.js
@@ -3,7 +3,7 @@ btn.addEventListener('click',() =>{
     let numbers = "kk";
 	var deleteFoodConfig = { headers: { 'Content-Type': 'text/html'} };  
     var reqFoodId = document.getElementById('fname').value;
-	axios.post('http://localhost:36000/testPost', reqFoodId,deleteFoodConfig)
+	axios.post('https://marcelo-spark-arep.herokuapp.com/testPost', reqFoodId,deleteFoodConfig)
         .then(res => {
             console.log(res);
 			alert(res.data);
