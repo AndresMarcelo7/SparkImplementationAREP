72d43e25fc22af1673df5bb1b6fb5af47eaacaad
Andres Marcelo <felipemarcelo156@gmail.com>
2020-09-01 19:04:26 -0500

    eliminacion 3.0

--- src/main/resources/action.js
+++ src/main/resources/action.js
@@ -1,17 +1,18 @@
 let btn = document.getElementById('buttonB');
-
 btn.addEventListener('click',() =>{
     let numbers = "kk";
-    axios.post('https://marcelo-spark-arep.herokuapp.com/testPost', numbers)
+	var deleteFoodConfig = { headers: { 'Content-Type': 'text/html'} };  
+    var reqFoodId = JSON.stringify({"123":"amigo"});
+axios.post('https://marcelo-spark-arep.herokuapp.com/testPost', reqFoodId,deleteFoodConfig)
         .then(res => {
             console.log("succesfull");
         })
         .catch( error =>{
             console.log(error);
         })
-    axios.post('http://localhost:36000/testPost', numbers)
+    axios.post('http://localhost:36000/testPost', reqFoodId,deleteFoodConfig)
         .then(res => {
-            doPost(res);
+            console.log("succesfull");
         })
         .catch( error =>{
             console.log(error);
