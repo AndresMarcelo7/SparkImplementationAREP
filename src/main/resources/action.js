22ff39eee16cca08581dec6d5ec62f824e3f79b6
Andres Marcelo <felipemarcelo156@gmail.com>
2020-09-01 21:08:34 -0500

    prueba

--- src/main/resources/action.js
+++ src/main/resources/action.js
@@ -1,15 +1,23 @@
 let btn = document.getElementById('buttonB');
 btn.addEventListener('click',() =>{
-    let numbers = "kk";
-	var deleteFoodConfig = { headers: { 'Content-Type': 'text/html'} };  
-    var reqFoodId = document.getElementById('fname').value;
-	axios.post('https://marcelo-spark-arep.herokuapp.com/testPost', reqFoodId,deleteFoodConfig)
+    /*let numbers = "kk";
+	var deleteFoodConfig = { headers: { 'Content-Type': 'application/json'} };  
+    var reqFoodId = document.getElementById('fname').value+"=hola";
+	axios.post('http://localhost:36000/testPost', reqFoodId,deleteFoodConfig)
         .then(res => {
             console.log(res);
 			alert(res.data);
         })
         .catch( error =>{
             console.log("Intentalo de nuevo");
+        })
+		*/
+		 $.ajax({
+          type: 'POST', url: 'https://marcelo-spark-arep.herokuapp.com/testPost', data: {
+            email: "test@email.com"
+          },
+        }).done(function(res) {
+          console.log(res);
         })
 
 });
